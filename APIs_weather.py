b3287b96fcf9e6c347749044736d17978255f07b
angu-8683 <angu-8683@csez.zohocorpin.com>
2020-05-15 17:09:20 +0530

    recent changes

--- APIs_weather.py
+++ APIs_weather.py
@@ -1,19 +1,6 @@
-# -*- coding: utf-8 -*- 
-
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 17 2015)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
-
 import wx
 import wx.xrc
 import requests
-
-###########################################################################
-## Class mainDialogBox
-###########################################################################
 
 class mainDialogBox ( wx.Dialog ):
     
